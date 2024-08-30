b4e825cc193e76b98e7f060acbacadaddc54d204
ricin9 <ricin9@outlook.com>
2024-08-30 14:09:25 +0100

    file cleanup

--- src/routes/private/+page.svelte
+++ src/routes/private/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import { page } from "$app/stores";
   import { onMount } from "svelte";
-  import Error from "../login/[magicToken]/+error.svelte";
 
   export let data;
 
--- src/routes/token-verify/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    const channel = new BroadcastChannel("my-channel");
-    channel.postMessage("yo, you are now authenticated, sup");
-  });
-</script>
